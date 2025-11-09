3f12f5b40a29e64964ef1f6de7018e7b4d0f1ef6
Ganesh <gp302999@gmail.com>
2025-11-09 22:14:27 +0530

    hello world message updated second time..

--- src/main/java/com/example/hello_world/controller/HelloWorldController.java
+++ src/main/java/com/example/hello_world/controller/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
     @GetMapping("/world")
     public String getMessage(){
-        return "Hello World!!!";
+        return "Hello World updated!!!";
     }
 
 }
